28f07c27c792f904aa39ef9263999aa9cb987683
Robson Rodrigues da Silva <robson_rodriguessilva@outlook.com>
2022-01-19 17:31:28 -0300

    Arquivos Postman

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AnalisarPostman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\robso\Estudos\Tecnologia\Postman Collections\";
+            string[] files = Directory.GetFiles(path, "*.json", SearchOption.TopDirectoryOnly);
+            List<Requisicoes> listaRequisicoes = new List<Requisicoes>();
+            int i = 1;
+            foreach (string file in files)
+            {
+                using(StreamReader r = new StreamReader(file))
+                {
+                    string json = r.ReadToEnd();
+                    JObject jsonCollection = JObject.Parse(json);
+                    int contAws = 0;
+                    foreach(var item in jsonCollection["item"])
+                    {
+                        if(item["request"]["url"]["raw"].ToString().IndexOf("viacep") > -1)
+                            contAws++;
+                    }
+                    Requisicoes requisicoes = new Requisicoes
+                    {
+                        RequisicaoId = i,
+                        RequisicaoName = Path.GetFileName(file),
+                        QuantidadeUrl = jsonCollection["item"].Count(),
+                        QuantidadeUrlAWS = contAws
+                    };
+                    listaRequisicoes.Add(requisicoes);
+                }
+                i++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Requisicoes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalisarPostman
+{
+    internal class Requisicoes
+    {
+        public int RequisicaoId {  get; set; }
+        public string RequisicaoName {  get; set; }
+        public int QuantidadeUrl { get; set; }
+        public int QuantidadeUrlAWS { get; set; }   
+    }
+}
